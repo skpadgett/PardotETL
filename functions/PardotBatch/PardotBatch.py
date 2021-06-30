8ceada3fb63b65b79dc325752ad3c866d405ea87
Clayton J Roberts <claytonjroberts@icloud.com>
2021-06-30 15:29:47 -0400

    WIP

--- functions/PardotBatch/PardotBatch.py
+++ functions/PardotBatch/PardotBatch.py
@@ -1,63 +1,83 @@
-import boto3 
+import boto3
 from boto3 import client
 from os import environ
 import asyncio
 import json
 
 # Pardot Task prefix
-tasks_prefix = environ['PardotTaskPrefix']
+TASKS_PREFIX = environ.get("PardotTaskPrefix")
 
 # Mapping of TaskGroups to Tasks to DataTypes
 # As of right now there is only one task group, but if some kind of postprocesing was required, this could handle it
-#   with a new group                 
-task_mapping = {"TaskGroup1":{"tasks":[
-                    {"task":"PardotPull", 
-                    "data_types":[ "visitoractivities","campaigns",
-                                    "lists","prospects","tagobjects","tags",
-                                    "opportunities","prospectaccounts","forms",
-                                    "listmemberships","visitors"]},          
-                    {"task":"PardotPullEmailStats",
-                    "data_types":["emailstats"]}]
-                    }
-                    }
+#   with a new group
+task_mapping = {
+    "TaskGroup1": {
+        "tasks": [
+            {
+                "task": "PardotPull",
+                "data_types": [
+                    "visitoractivities",
+                    "campaigns",
+                    "lists",
+                    "prospects",
+                    "tagobjects",
+                    "tags",
+                    "opportunities",
+                    "prospectaccounts",
+                    "forms",
+                    "listmemberships",
+                    "visitors",
+                ],
+            },
+            {"task": "PardotPullEmailStats", "data_types": ["emailstats"]},
+        ]
+    }
+}
 
 
-lambda_client = client('lambda')
+lambda_client = client("lambda")
+
 
 def execute_tasks(task_dict, lambda_client):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(execute_task_group_async(task_dict, lambda_client))
-    return 
+    return
+
 
 async def execute_task_group_async(task_dict, client):
-    '''
+    """
     Asynchronously executes task / data_type collection as a group
-    Have to reformat the dictionary into a list to allow asyncio gather to work  
-    '''
-    tasks = [[[task,data_type] for data_type in task_dict[task]] for task in task_dict.keys()][0]
-    return await asyncio.gather(*[invoke_function(task[0], task[1],client) for task in tasks])
+    Have to reformat the dictionary into a list to allow asyncio gather to work
+    """
+    tasks = [
+        [[task, data_type] for data_type in task_dict[task]]
+        for task in task_dict.keys()
+    ][0]
+    return await asyncio.gather(
+        *[invoke_function(task[0], task[1], client) for task in tasks]
+    )
 
 
 async def invoke_function(task, data_type, lambda_client):
-    '''
+    """
     Invokes lambda function for all for a specific task, for a specific type of data
-    '''
-    payload = {'Metadata': {'DataType': data_type}}
+    """
+    payload = {"Metadata": {"DataType": data_type}}
 
     response = lambda_client.invoke(
-            FunctionName=task,
-            InvocationType='RequestResponse',
-            Payload=json.dumps(payload))
-    
+        FunctionName=task, InvocationType="RequestResponse", Payload=json.dumps(payload)
+    )
+
     return response
 
 
 def lambda_handler(event, context):
-    
-    print(f'Starting Pardot Batch pull')
+
+    print(f"Starting Pardot Batch pull")
 
     execute()
+
 
 def execute():
     def PardotBatch():
@@ -66,7 +86,9 @@ def execute():
             for tasks in task_mapping[taskgroup]["tasks"]:
                 for task in tasks.keys():
                     # Update the task name to match the lambda name (ie. pardot-us-east-2-prod.. might get added)
-                    updated_dict[f'{tasks_prefix}-{tasks["task"]}'] = tasks['data_types']
+                    updated_dict[f'{TASKS_PREFIX}-{tasks["task"]}'] = tasks[
+                        "data_types"
+                    ]
             execute_tasks(updated_dict, lambda_client)
 
     PardotBatch()
--- functions/PardotPull/PardotPull.py
+++ functions/PardotPull/PardotPull.py
@@ -1,186 +1,173 @@
 from pypardot.client import PardotAPI
-import pandas as pd							  
-import boto3                                  
+import pandas as pd
+import boto3
 import time
 import json
-from datetime import datetime
+import datetime as dt
 from time import sleep
 from os import environ
 from operator import itemgetter
 from smart_open import smart_open
 
-#TODO, figure out if that nested while loop works
-#TODO, setup exception handler for batch and for task
-#TODO, instead of using max ID, need to use max update datetime from the data to filter pardot. 
-    # I guess just query the data and keep track of the max time to store in a file
-#TODO, need to have a query of snowflake if a max doesn't exist in the bucket
-#TODO, maybe also have it check snowflake and take the larger value of the two incase an error happens with S3 write
-    # its ok to have duplicates, snowflake handles. But not ideal
+# TODO, figure out if that nested while loop works
+# TODO, setup exception handler for batch and for task
+# TODO, instead of using max ID, need to use max update datetime from the data to filter pardot.
+# I guess just query the data and keep track of the max time to store in a file
+# TODO, need to have a query of snowflake if a max doesn't exist in the bucket
+# TODO, maybe also have it check snowflake and take the larger value of the two incase an error happens with S3 write
+# its ok to have duplicates, snowflake handles. But not ideal
 # required format for 'update_after' in pardot query -> 2021-04-15 00:00:01
 
 # Gather environmental variables
-email = environ.get('pardotEmail')
-user_key = environ.get('pardotUserKey')
-sf_consumer_key = environ.get('pardotSfConsumerKey')
-sf_consumer_secret = environ.get('pardotSfConsumerSecret')
-sf_refresh_token = environ.get('pardotSfRefreshToken')
-business_unit_id = environ.get('pardotSfBusinessUnitID')
-version = environ.get('pardotVersion')
-bucket = environ.get('s3FileStore')
+EMAIL = environ.get("pardotEmail")
+USER_KEY = environ.get("pardotUserKey")
+SF_CONSUMER_KEY = environ.get("pardotSfConsumerKey")
+SF_CONSUMER_SECRET = environ.get("pardotSfConsumerSecret")
+SF_REFRESH_TOKEN = environ.get("pardotSfRefreshToken")
+BUSINESS_UNIT_ID = environ.get("pardotSfBusinessUnitID")
+VERSION = environ.get("pardotVersion")
+BUCKET = environ.get("s3FileStore")
+SECONDS_TIMEOUT: int = 60 * 13  # 13 Minutes
+
 
 # Placeholder values
-maxid=0
-i=0
-last_modified_date = datetime(1939, 9, 1).replace(tzinfo=None)
+
 
 def lambda_handler(event, context):
-    
-    # Time lambda started 
-    start_time = datetime.now()
+
+    # Time lambda started
+    start_time = dt.datetime.now()
 
     # The type of data being pulled
-    data_type = event['Metadata'].get('DataType')
+    data_type = event["Metadata"].get("DataType")
 
-    print(f'Starting Pardot pull for {data_type}')
+    print(f"Starting Pardot pull for {data_type}")
 
     # Timeout for loop pulling data.
     # We need it to be less than 15 so the batch lambda doesn't time out
-    timeout = time.time() + 60*13
+    timeout = time.time() + SECONDS_TIMEOUT
 
     # Execute the function and pull out the total length of data pulled
-    data_length = execute(data_type,timeout)
+    data_length, last_modified_date = execute(data_type, timeout)
 
     # Time lambda finished
-    end_time = datetime.now()
-    
-    print(f"""Finished Pardot pull for {data_type}. 
+    end_time = dt.datetime.now()
+
+    print(
+        f"""Finished Pardot pull for {data_type}.
     Process took {end_time-start_time} seconds to pull.
-    {data_length} were pulled""")
+    {data_length} were pulled"""
+    )
 
     # Return results to batch script
     # TODO We should have the response return that max last_modified_date found in the data
     return {
-        'statusCode': 200,
-        'body': {'DataType': data_type, 'Result': 'Successful', 'NumberofRecords': data_length,
-        'RunTime':str(end_time-start_time)}
+        "statusCode": 200,
+        "body": {
+            "DataType": data_type,
+            "Result": "Successful",
+            "NumberofRecords": data_length,
+            "RunTime": str(end_time - start_time),
+            "LastModifiedDate": str(last_modified_date.isoformat()),
+        },
     }
 
-def execute(data_type,timeout):
+
+def execute(data_type, timeout) -> (int, dt.datetime):
+
     # Create class for writing to s3 bucket
-    def pull_data(data_type,timeout):
-        class S3JsonBucket:
-            def __init__(self, bucket_name):
-                self.bucket = boto3.resource("s3").Bucket(bucket_name)
+    class S3JsonBucket:
+        def __init__(self, bucket_name):
+            self.bucket = boto3.resource("s3").Bucket(bucket_name)
 
-            def load(self, key):
-                return json.load(self.bucket.Object(key=key).get()["Body"])
+        def load(self, key):
+            return json.load(self.bucket.Object(key=key).get()["Body"])
 
-            def dump(self, key, obj):
-                return self.bucket.Object(key=key).put(Body=json.dumps(obj))
+        def dump(self, key, obj):
+            return self.bucket.Object(key=key).put(Body=json.dumps(obj))
 
-        #Instantiate Pardot Client
-        p=PardotAPI(email=email,user_key=user_key,sf_consumer_key=sf_consumer_key,sf_consumer_secret=sf_consumer_secret,sf_refresh_token=sf_refresh_token,business_unit_id=business_unit_id,version=version)
+    # Instantiate Pardot Client
+    p = PardotAPI(
+        email=EMAIL,
+        user_key=USER_KEY,
+        sf_consumer_key=SF_CONSUMER_KEY,
+        sf_consumer_secret=SF_CONSUMER_SECRET,
+        sf_refresh_token=SF_REFRESH_TOKEN,
+        business_unit_id=BUSINESS_UNIT_ID,
+        version=VERSION,
+    )
 
-        # Determine what needs to be queried based off type of data
-        if data_type == 'visitoractivities':
-            data_client = p.visitoractivities
+    try:
+        data_client = getattr(p, data_type)
+    except AttributeError:
+        raise Exception(f"{data_type} is not a valid selection!")
 
-        elif data_type == 'campaigns':
-            data_client = p.campaigns
+    # Do we need separate ones for different files?
+    jsbucketMaxID = boto3.resource("s3").Bucket(BUCKET)
 
-        elif data_type == 'lists':
-            data_client = p.lists
+    # TODO, this needs to be re-written to focus on max modified date instead of MAXID
+    # TODO, we also might have the batch job handle this process
+    # TODO and pass as a task parameter
 
-        elif data_type == 'prospects':
-            data_client = p.prospects
+    # Iterate through files to get the latest max id
+    maxIdFiles = jsbucketMaxID.objects.filter(Prefix=f"{data_type}/maxid")
+    last_modified_date = dt.datetime(1939, 9, 1)
 
-        elif data_type == 'tagobjects':
-            data_client = p.tagobjects
+    if len(maxIdFiles) > 0:
+        last_modified_file = max(maxIdFiles, key=lambda k: k["last_modified"])
 
-        elif data_type == 'tags':
-            data_client = p.tags
+        last_modified_date = last_modified_file["last_modified"]
+        last_modified_file_key = last_modified_file["file"].key
 
-        elif data_type == 'opportunities':
-            data_client = p.opportunities
+        # Initialize maxid from retrieved file
+        with smart_open(f"s3://{BUCKET}/{last_modified_file_key}") as fh:
+            maxid = json.load(fh)
 
-        elif data_type == 'prospectaccounts':
-            data_client = p.prospectaccounts
+        print("initial maxid=", maxid)
+    else:
+        # if a max id file doesn't exist yet, then start at 0
+        maxid = 0
 
-        elif data_type == 'forms':
-            data_client = p.forms
+    jsbucketDump = S3JsonBucket(BUCKET)
+    total_records = 0
 
-        elif data_type == 'listmemberships':
-            data_client = p.listmemberships
+    # So we want this to stop running after 13 minutes. We want to guarentee that batch runner doesn't timeout
+    # waiting for this
+    # TODO, figure out if this while loop works
 
-        elif data_type == 'visitors':
-            data_client = p.visitors
+    i = 0
 
-        else :
-            
-            raise Exception (f'{data_type} is not a valid selection!')
+    while (time.time() < timeout) and (
+        i <= data_client.query(id_greater_than=maxid)["total_results"] - 1
+    ):
+        # This query needs to be changed to us last_updated_dt. We also want to have this passed
+        # as a parameter by the batch
 
-        # Do we need separate ones for different files?
-        jsbucketMaxID=boto3.resource("s3").Bucket(bucket)
+        data = data_client.query(format="json", sort_by="id", id_greater_than=maxid)
 
-        # TODO, this needs to be re-written to focus on max modified date instead of MAXID
-        # TODO, we also might have the batch job handle this process
-        # TODO and pass as a task parameter
-        #Iterate through files to get the latest max id
-        maxIdFiles = jsbucketMaxID.objects.filter(Prefix=f'{data_type}/maxid')
-        if len(maxIdFiles)>0:
-            modified_dates = [{'file':file,'last_modified':file['last_modified'].replace(tzinfo=None)} for file in maxIdFiles]
-            last_modified_date = max(modified_dates['last_modified'], key=lambda ev: ev['last_modified'])
-            last_modified_file_key = [file['file'] for file in modified_dates if file['last_modified']==last_modified_date][0].key
-            # Initialize maxid from retrieved file
-            path='s3://{}/{}'.format(bucket, last_modified_file_key)
-            maxid = json.load(smart_open(path))
-            print('initial maxid=',maxid)
-        else:
-            # if a max id file doesn't exist yet, then start at 0
-            maxid = 0
-        
-        jsbucketDump = S3JsonBucket(bucket)
-        total_records = 0
+        writetime = time.strftime("%Y%m%d-%H%M%S")
 
-        # So we want this to stop running after 13 minutes. We want to guarentee that batch runner doesn't timeout
-        # waiting for this
-        #TODO, figure out if this while loop works
-        while True:
-            
-            # This query needs to be changed to us last_updated_dt. We also want to have this passed
-            # as a parameter by the batch
-            while i <= data_client.query(id_greater_than=maxid)['total_results'] -1: 
+        file_name = f"test/{data_type}/{data_type}_{writetime}.json"
+        jsbucketDump.dump(file_name, data)
 
-                data=data_client.query(format='json',sort_by='id',id_greater_than=maxid)
+        print(f"Sent {file_name} to {BUCKET} with {len(data.keys())} records")
 
-                writetime=time.strftime("%Y%m%d-%H%M%S") 
+        writetime = time.strftime("%Y%m%d-%H%M%S")
+        jsbucketDump.dump(f"{data_type}/{data_type}_{writetime}.json", data)
 
-                file_name = f"test/{data_type}/{data_type}_{writetime}.json"
-                jsbucketDump.dump(file_name, data)
+        # write current maxid to S3 - If lambda times out, next execution will pickup with this value of maxid.
+        maxid = data[data_type][-1]["id"]
+        jsbucketDump.dump(f"{data_type}_maxid/" + "maxid_" + writetime + ".json", maxid)
+        print(f"updated maxid={maxid}")
 
-                print(f'Sent {file_name} to {bucket} with {len(data.keys())} records')
+        sleep(1)  # 1 sec delay to insure different filenames
 
-                writetime=time.strftime("%Y%m%d-%H%M%S") 
-                jsbucketDump.dump(f"{data_type}/{data_type}_{writetime}.json", data)
+        i = i + 200
 
-                # write current maxid to S3 - If lambda times out, next execution will pickup with this value of maxid.
-                maxid=data[data_type][-1]['id']
-                jsbucketDump.dump(f"{data_type}_maxid/" + "maxid_" + writetime + ".json", maxid)
+        total_records = total_records + len(data.keys())
 
-                print('updated maxid=',maxid)
-                sleep(1)   # 1 sec delay to insure different filenames
-                i=i+200
+        if len(data.keys()) < 200 or time.time() > timeout:
+            # Timeout is hit or the number of records indicates the end, break loop
+            break
 
-                total_records = total_records + len(data.keys())
-
-                if len(data.keys()) < 200 or time.time() > timeout:
-                    break
-
-            if time.time() > timeout:    
-                break
-
-        return total_records
-
-    return pull_data(data_type)
-
-      
+    return total_records, last_modified_date
